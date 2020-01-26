2913277420bd1eae4d569a8588ce26cc963b846f
harimambura <harimambura@gmail.com>
2020-01-27 01:06:39 +0300

    Update app

--- app.js
+++ app.js
@@ -1,48 +1,41 @@
 'use strict';
 
-var jade = require('jade'),
-    _ = require('lodash'),
-    wld;
+const jade = require('pug');
+const _ = require('lodash');
+let wld;
 
-module.exports = function (app, logger) {
+module.exports = function(app, logger) {
 
-    wld = new (require('./lib/wld'))(logger);
+	wld = new (require('./lib/wld'))(logger);
 
-    app.get('/', function(req, res) {
-        res.redirect('/logs/1');
-    });
+	app.get('/logs', function(req, res) {
+		res.redirect('/logs/1');
+	});
 
-    app.get('/logs', function(req, res) {
-        res.redirect('/logs/1');
-    });
+	app.get('/logs/:page', function(req, res) {
+		let page = parseInt(req.params.page) || 1;
+		const itemsOnPage = 30;
+		const level = req.query.level || '';
 
-    app.get('/logs/:page', function(req, res) {
-        var page = req.params.page || 1,
-            itemsOnPage = 30;
+		wld.list({
+			limit: itemsOnPage,
+			offset: (page - 1) * itemsOnPage,
+			level: level
+		}).then(function(logs) {
+			const pagesCount = Math.floor(wld.count / itemsOnPage) + (wld.count % itemsOnPage === 0 ? 0 : 1);
 
-        page = parseInt(page);
+			const start = Math.max(1, page - 2);
+			const end = Math.min(pagesCount, start + 4);
 
-        var level = req.query.level || '';
+			res.send(jade.renderFile(__dirname + '/views/logs.jade', {
+				logs: logs,
+				pages: _.range(start, end + 1),
+				currentPage: page,
+				lastPage: pagesCount,
+				level: level
+			}, null));
+		});
 
-        wld.list({
-            limit: itemsOnPage,
-            offset: (page - 1) * itemsOnPage,
-            level: level
-        }).then(function (logs) {
-            var pagesCount = Math.floor(wld.count / itemsOnPage) + (wld.count % itemsOnPage === 0 ? 0 : 1);
-
-            var start = Math.max(1, page - 2);
-            var end = Math.min(pagesCount, start + 4);
-
-            res.send(jade.renderFile(__dirname + '/views/logs.jade', {
-                logs: logs,
-                pages: _.range(start, end + 1),
-                currentPage: page,
-                lastPage: pagesCount,
-                level: level
-            }, null));
-        });
-
-    });
+	});
 
 };
